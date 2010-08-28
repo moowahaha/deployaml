53f179c21941af22b24ae3372bcc03b7f3b76e43
Stephen Hardisty <moowahaha@hotmail.com>
2010-08-28 19:04:25 +1000

    Even more refactoring

--- lib/deployaml/scm/git.rb
+++ lib/deployaml/scm/git.rb
@@ -4,17 +4,20 @@ module Deployaml
   module Scm
     class Git < ScmBase
       def fetch_files
-        result = `git clone --depth=1 #{repository_path} #{staging_path} 2>&1`
-
-        if result =~ /fatal:/
-          raise "Cannot read repository #{repository_path}" if result =~ /not a git archive/ || result =~ /Could not switch to/
-          raise "Error cloning #{repository_path}: #{result}"
-        end
+        cloned?(`git clone --depth=1 #{repository_path} #{staging_path} 2>&1`)
 
         `cd #{staging_path} && git submodule init && git submodule update`
       end
 
+      def cloned? result
+        if result =~ /fatal:/
+          raise "Cannot read repository #{repository_path}" if result =~ /not a git archive/ || result =~ /Could not switch to/
+          raise "Error cloning #{repository_path}: #{result}"
+        end
+      end
+
       def clean
+        # remove all the gittish files
         Dir.glob(File.join(staging_path, '**', '.git*')).each do |git_file|
           FileUtils.rm_r(git_file)
         end
