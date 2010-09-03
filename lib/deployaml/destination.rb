87b9fa092470a15daffefda2661d3c9738b38e24
Stephen Hardisty <moowahaha@hotmail.com>
2010-09-03 17:48:34 +1000

    Spruced up remote commands

--- lib/deployaml/destination.rb
+++ lib/deployaml/destination.rb
@@ -1,5 +1,6 @@
 require 'net/ssh'
 require 'net/scp'
+require 'open3'
 
 module Deployaml
   class Destination
@@ -11,22 +12,76 @@ module Deployaml
     end
 
     def install_from local_path
-          FileUtils.mkdir_p(File.join(path, 'releases'))
+      FileUtils.mkdir_p(File.join(path, 'releases'))
 
-          destination_path = File.join(path, 'releases', Time.now.strftime('%Y%M%d%H%M%S'))
-          current_symlink = File.join(path, 'current')
+      destination_path = File.join(path, 'releases', Time.now.strftime('%Y%M%d%H%M%S'))
+      current_symlink = File.join(path, 'current')
 
-          FileUtils.cp_r(
-                  local_path,
-                  destination_path
-          )
+      FileUtils.cp_r(
+              local_path,
+              destination_path
+      )
 
-          File.unlink(current_symlink) if File.exists?(current_symlink)
-          File.symlink(destination_path, current_symlink)
+      File.unlink(current_symlink) if File.exists?(current_symlink)
+      File.symlink(destination_path, current_symlink)
+    end
+
+    def local_exec(command, extra_command, ok_message)
+      had_error = true
+      output = ''
+
+      Open3.popen3(command + extra_command) do |stdin, stdout, stderr|
+        stdout_output, stderr_output = stdout.read, stderr.read
+
+        if !stdout_output.empty?
+          if stdout_output.gsub!(/#{ok_message}\n/, '')
+            had_error = false
+          end
+
+          output += stdout_output
+          $stdout.print stdout_output
+        end
+
+        if !stderr_output.empty?
+          output += stderr_output
+          $stderr.print stderr_output
+        end
+      end
+
+      return had_error, output
+    end
+
+    def remote_exec(command, extra_command, ok_message)
+      had_error = true
+      output = ''
+
+      @session.exec!(command + extra_command) do |ch, stream, data|
+        if stream == :stderr
+          $stderr.print data
+          output += data
+        else
+          if data.strip == ok_message
+            had_error = false
+          else
+            $stdout.print data
+            output += data
+          end
+        end
+      end
+
+      return had_error, output
     end
 
     def exec command
-      @session ? @session.exec!(command) : `#{command} 2>&1`
+      ok_message = rand.to_s
+
+      extra_command = " && echo #{ok_message}"
+
+      had_error, output = self.send((@session ? 'remote_exec' : 'local_exec'), command, extra_command, ok_message)
+
+      raise "Error executing '#{command}'" if had_error
+
+      return output
     end
 
     private
--- spec/destination_spec.rb
+++ spec/destination_spec.rb
@@ -12,32 +12,54 @@ describe Deployaml::Destination do
   end
 
   context "local destination" do
-    before do
-      @destination_dir = '/tmp/destination_spec_test_dir'
-      FileUtils.rm_r(@destination_dir) if File.exists?(@destination_dir)
+    context "installing" do
+      before do
+        @destination_dir = '/tmp/destination_spec_test_dir'
+        FileUtils.rm_r(@destination_dir) if File.exists?(@destination_dir)
 
-      fake_time = mock('time')
-      Time.should_receive(:now).and_return(fake_time)
-      fake_time.should_receive(:strftime).with('%Y%M%d%H%M%S').and_return('20100901200900')
+        fake_time = mock('time')
+        Time.should_receive(:now).and_return(fake_time)
+        fake_time.should_receive(:strftime).with('%Y%M%d%H%M%S').and_return('20100901200900')
 
-      destination = Deployaml::Destination.new('path' => @destination_dir)
-      destination.install_from "#{File.dirname(__FILE__)}/../fixtures/local_deployment_test_project"
+        destination = Deployaml::Destination.new('path' => @destination_dir)
+        destination.install_from "#{File.dirname(__FILE__)}/../fixtures/local_deployment_test_project"
+      end
+
+      it "should copy a staging directory to a release directory" do
+        File.should be_directory("/tmp/destination_spec_test_dir/releases/20100901200900")
+        File.should exist("/tmp/destination_spec_test_dir/releases/20100901200900/harold.txt")
+      end
+
+      it "should symlink the current runner to the spanking new release" do
+        File.should be_symlink("/tmp/destination_spec_test_dir/current")
+        File.should exist("/tmp/destination_spec_test_dir/current/harold.txt")
+      end
     end
 
-    it "should copy a staging directory to a release directory" do
-      File.should be_directory("/tmp/destination_spec_test_dir/releases/20100901200900")
-      File.should exist("/tmp/destination_spec_test_dir/releases/20100901200900/harold.txt")
+    context "shell commands" do
+      before do
+        @destination = Deployaml::Destination.new('path' => '/tmp/destination_spec_test_dir')
+      end
+
+      it "should return the result of the shell command" do
+        @destination.exec('pwd').should == Dir.pwd + "\n"
+      end
+
+      it "should raise an exception when something does wrong" do
+        lambda { @destination.exec('aslkjsdf') }.should raise_error("Error executing 'aslkjsdf'")
+      end
+
+      it "should print output to stdout" do
+        $stdout.should_receive(:print).with(Dir.pwd + "\n")
+        @destination.exec('pwd').should == Dir.pwd + "\n"
+      end
+
+      it "should print errors to stderr" do
+        $stderr.should_receive(:print).with(/.+: aslkjsdf: command not found\n/)
+        lambda { @destination.exec('aslkjsdf') }.should raise_error("Error executing 'aslkjsdf'")
+      end
     end
 
-    it "should symlink the current runner to the spanking new release" do
-      File.should be_symlink("/tmp/destination_spec_test_dir/current")
-      File.should exist("/tmp/destination_spec_test_dir/current/harold.txt")
-    end
-
-    it "should execute a shell command" do
-      destination = Deployaml::Destination.new('path' => @destination_dir)
-      destination.exec('pwd').should == Dir.pwd + "\n"
-    end
   end
 
   context "remote destination" do
@@ -51,6 +73,62 @@ describe Deployaml::Destination do
       )
 
       destination.exec('whoami').should == remote['username'] + "\n"
+    end
+
+#    context "installing" do
+#      before do
+#        @destination_dir = '/tmp/destination_spec_test_dir'
+#        FileUtils.rm_r(@destination_dir) if File.exists?(@destination_dir)
+#
+#        fake_time = mock('time')
+#        Time.should_receive(:now).and_return(fake_time)
+#        fake_time.should_receive(:strftime).with('%Y%M%d%H%M%S').and_return('20100901200900')
+#
+#        destination = Deployaml::Destination.new('path' => @destination_dir)
+#        destination.install_from "#{File.dirname(__FILE__)}/../fixtures/local_deployment_test_project"
+#      end
+#
+#      it "should copy a staging directory to a release directory" do
+#        File.should be_directory("/tmp/destination_spec_test_dir/releases/20100901200900")
+#        File.should exist("/tmp/destination_spec_test_dir/releases/20100901200900/harold.txt")
+#      end
+#
+#      it "should symlink the current runner to the spanking new release" do
+#        File.should be_symlink("/tmp/destination_spec_test_dir/current")
+#        File.should exist("/tmp/destination_spec_test_dir/current/harold.txt")
+#      end
+#    end
+
+    context "shell commands" do
+      before(:all) do
+        @remote = YAML.load_file(
+                File.dirname(__FILE__) + '/../stuff_not_to_be_committed/host_and_username_with_ssh_keys.yml'
+        )
+
+        @destination = Deployaml::Destination.new(
+                {
+                        'path' => '/tmp/blah'
+                }.merge(@remote)
+        )
+      end
+
+      it "should return the result of the shell command" do
+        @destination.exec('hostname').should_not == `hostname`
+      end
+
+      it "should raise an exception when something does wrong" do
+        lambda { @destination.exec('aslkjsdf') }.should raise_error("Error executing 'aslkjsdf'")
+      end
+
+      it "should print output to stdout" do
+        $stdout.should_receive(:print).with(@remote['username'] + "\n")
+        @destination.exec('whoami').should == @remote['username'] + "\n"
+      end
+
+      it "should print errors to stderr" do
+        $stderr.should_receive(:print).with(/.+: aslkjsdf: command not found\n/)
+        lambda { @destination.exec('aslkjsdf') }.should raise_error("Error executing 'aslkjsdf'")
+      end
     end
   end
 end
